8113b8d39cc048a2150a01c1a5f1b3efcb42bdf1
Mabel890 <mabelvillada890@gmail.com>
2021-07-15 12:28:53 -0500

    Creación de archivos

---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <input type="email" v-model="email" />
+    <input type="password" v-model="password" />
+    <button @click="login()">Log in</button>
+    <router-link to="/register">Sign up</router-link>
+    <p>{{ getData }}</p>
+  </div>
+</template>
+
+<script>
+const axios = require("axios");
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+      getData: {},
+    };
+  },
+  methods: {
+    async login() {
+      // console.log(`${this.email} ${this.password}`)
+      const dataGet = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+      console.log(dataGet);
+      this.getData = dataGet.data;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" v-model="email" />
+    <input type="password" v-model="password" />
+    <button @click="click()">Sign up</button>
+    <router-link to="/login">Log in</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    click() {
+      console.log(`${this.email} ${this.password}`);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
